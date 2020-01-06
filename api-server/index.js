2523e9437e49b425315ae3ca198b86d0f6a916d1
kevinconvery <kevinconvery@gmail.com>
2020-01-05 20:46:41 -0500

    add body-parser, work on Register view

--- api-server/index.js
+++ api-server/index.js
@@ -3,8 +3,10 @@ const app = express();
 const mountRoutes = require('./routes');
 const port = 4000;
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({ extended: false }));
 mountRoutes(app);
 
 app.get('/', (req, res) => res.send("Hello From Sock Palace!"));
--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -6,39 +6,129 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-
-
 const Register = props => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [province, setProvince] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+
+  const handleSubmitRegister = event => {
+    event.preventDefault();
+    alert(`Username submitted is ${username}`);
+  }
+  
   return (
     <div>
       <Container>
         <Jumbotron>
-          <h1>Register with us to create orders and review items!</h1>
+          <h2>Register with us to create orders and review items!</h2>
         </Jumbotron>
-        <Row className="justify-content-center">
-          <Col className="col-8">
-            <Form>
+        <Form onSubmit={handleSubmitRegister}>
+          <Form.Row className="d-flex justify-content-center">
+            <Col lg={4}>
+              <Form.Group controlId="formBasicUsername">
+                <Form.Label className="d-flex justify-content-start mx-2 mb-2">User name</Form.Label>
+                <Form.Control
+                  required
+                  type="text" 
+                  placeholder="Enter user name"
+                  value={username}
+                  onChange={e => setUsername(e.target.value)}  
+                />
+              </Form.Group>
+            </Col>
+            <Col lg={6}>
               <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
+                <Form.Label className="d-flex justify-content-start mx-2 mb-2">Email address</Form.Label>
+                <Form.Control
+                  required
+                  type="email" 
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}  
+                />
               </Form.Group>
-
+            </Col>
+          </Form.Row>
+          <Form.Row className="d-flex justify-content-center">
+            <Col lg={10}>
+              <Form.Group controlId="formBasicAddress">
+                <Form.Label className="d-flex justify-content-start mx-2 mb-2">Address</Form.Label>
+                <Form.Control
+                  required
+                  type="text" 
+                  placeholder="Address"
+                  value={address}
+                  onChange={e => setAddress(e.target.value)}
+                />    
+              </Form.Group>
+            </Col>
+          </Form.Row>
+          <Form.Row className="d-flex justify-content-center">
+            <Col lg={10}>
               <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Label className="d-flex justify-content-start mx-2 mb-2">Password</Form.Label>
+                <Form.Control
+                  required
+                  type="password" 
+                  placeholder="Password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
               </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+            </Col>
+          </Form.Row>
+          <Form.Row className="d-flex justify-content-center">
+            <Col lg={4}>
+              <Form.Group controlId="formBasicCity">
+                <Form.Label className="d-flex justify-content-start mx-2 mb-2">City</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="City"
+                  value={city}
+                  onChange={e => setCity(e.target.value)}
+                />
               </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Col>
-        </Row>
+            </Col>
+            <Col lg={4}>
+              <Form.Group controlId="formBasicProvince">
+                <Form.Label className="d-flex justify-content-start mx-2 mb-2">Province</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Province"
+                  value={province}
+                  onChange={e => setProvince(e.target.value)}
+                />
+              </Form.Group>              
+            </Col>
+            <Col lg={2}>
+              <Form.Group controlId="formBasicPostalCode">
+                <Form.Label className="d-flex justify-content-start mx-2 mb-2">Postal code</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Postal code"
+                  value={postalCode}
+                  onChange={e => setPostalCode(e.target.value)}
+                />
+              </Form.Group>              
+            </Col>
+          </Form.Row>
+          <Form.Row className="d-flex justify-content-center">
+            <Button
+              size="lg"
+              variant="primary" 
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Form.Row>
+        </Form>
       </Container>
     </div>
   )
